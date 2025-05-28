42a0a869e13fcb530abdd43fd591724cafc3ec84
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2025-05-28 18:08:07 +0300

    style: update class header content

--- src/test/java/com/iqkv/servicename/TechnicalStructureTest.java
+++ src/test/java/com/iqkv/servicename/TechnicalStructureTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 IQKV Team.
+ * Copyright 2025 IQKV Foundation Team.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
